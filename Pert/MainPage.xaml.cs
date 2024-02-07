c8452484688e70812c1267c428d8f872ea869a05
Alessio Catrini <catrini.alessio2@gmail.com>
2024-02-08 00:36:33 +0100

    Aggiunte nuove attività di test Implementata logica migliore posizionamento cerchi nelle colonne Aggiunta label all'interno del cerchio

--- Pert/MainPage.xaml.cs
+++ Pert/MainPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Maui.Controls;
-using Microsoft.Maui.Controls.Shapes;
+﻿using Microsoft.Maui.Controls.Shapes;
 
 namespace Pert
 {
@@ -16,15 +15,18 @@ namespace Pert
         void Gianni()
         {
             float screenWidth = 500;
-            Attività a = new('a', null);
+            Attività a = new('a', []);
             Attività b = new('b', [a]);
             Attività c = new('c', [a]);
             Attività d = new('d', [a]);
+            Attività e = new('e', [c]);
+            Attività f = new('f', [b]);
+            Attività g = new('g', [f]);
 
 
-            Attività = [a, b, c, d];
+            Attività = [a, b, c, d, e, f,g];
 
-            var AttivitàDivisePerPriorità = Attività.GroupBy(n => string.Join(" ", n.Priorità ?? []));
+            var AttivitàDivisePerPriorità = Attività.GroupBy(n => string.Join(" ", n.Priorità.Select(x => x.Lettera) ?? []));
             foreach (var x in Attività)
             {
                 var m = AttivitàDivisePerPriorità.Where(j => j.Contains(x)).ToList()[0];
@@ -55,16 +57,57 @@ namespace Pert
 
         void FaiComparireCerchi()
         {
-            int nColonne = test(Attività.Select(x => x.Siblings.Count+1).ToList());
-            for (int i = 0; i < nColonne; i++)
+            int nRighe = 3;
+            for (int i = 0; i < nRighe; i++)
                 Tia.ColumnDefinitions.Add(new ColumnDefinition(GridLength.Star));
-            for (int i = 0; i< Attività.Count; i++)
-                Tia.RowDefinitions.Add(new RowDefinition(GridLength.Star));
 
-            //Guarda su che colonna è la più lontana delle priorità e vai nella colonna dopo
-           
 
-            //Crea un grafo in un qualche modo e infilali  in qualche modo
+            for (int i = 0; i < Attività.Count; i++)
+                Tia.ColumnDefinitions.Add(new ColumnDefinition(GridLength.Star));
+
+            Attività.Sort((x, y) => x.Priorità.Count.CompareTo(y.Priorità.Count));
+            foreach (var attività in Attività)
+            {
+                if (attività.Priorità.Any())
+                {
+                    attività.NColonna = attività.Priorità.Select(x => x.NColonna).Max() + 1;
+                }
+                else
+                {
+                    attività.NColonna = 0;
+                }
+            }
+
+            {
+                foreach (var attività in Attività)
+                {
+                    if (attività.Siblings.Count == 0)
+                    {
+                        if (attività.Priorità.Count != 0)
+                            attività.NRiga = (int)attività.Priorità.Select(x => x.NRiga).Average();
+                        else
+                            attività.NRiga = (int)Math.Round((double)nRighe / 2) - 1; //Return center??
+                    }
+                    else
+                    {
+                        List<Attività> lista = [attività];
+                        lista.AddRange(attività.Siblings);
+                        lista.Sort((x, y) => x.Center.x.CompareTo(y.Center.x));
+                        attività.NRiga = lista.IndexOf(lista.First(x => x.Center.x == attività.Center.x));
+                    }
+                }
+            }
+            foreach (var attività in Attività)
+            {
+                var cerchio = new Ellipse() { WidthRequest = 100, HeightRequest = 100, BackgroundColor = Colors.Red, HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center };
+                //Da togliere
+                Label l = new() { Text = attività.Lettera.ToString().ToUpper(), HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, TextColor = Colors.White, FontSize = 20 };
+                
+                    Tia.Add(cerchio, attività.NColonna, attività.NRiga);    //Se ha più fratelli allora gestisci
+                    Tia.Add(l, attività.NColonna, attività.NRiga);
+                
+            }
+
         }
         static int gcd(int n1, int n2)
         {
@@ -78,9 +121,10 @@ namespace Pert
             }
         }
 
-        public static int test(List<int> numbers)
+
+        public static int NumeroPrimoPrecedente(int n)
         {
-            return numbers.Aggregate((S, val) => S * val / gcd(S, val));
+            return 2;
         }
     }
 }
